69c146a3505475d4a49b75b26f16f5aaf191bb52
leo <leoaee@gmail.com>
2021-09-13 12:53:12 +0800

    feat: update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,59 +5,43 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>小猪</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
+          Welcome to <a href="https://xiaozhu.dev">小猪</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <p className={styles.description}>👋</p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://xiaozhu.dev" className={styles.card}>
+            <h3>click 👆 &rarr;</h3>
+            <p>东</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="https://xiaozhu.dev" className={styles.card}>
+            <h3>click 👆 &rarr;</h3>
+            <p>南</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="https://xiaozhu.dev" className={styles.card}>
+            <h3>click 👆 &rarr;</h3>
+            <p>西</p>
           </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://xiaozhu.dev" className={styles.card}>
+            <h3>click 👆 &rarr;</h3>
+            <p>北</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        <a href="https://xiaozhu.dev" target="_blank" rel="noopener noreferrer">
+          © {new Date().getFullYear()}, Built with Nextjs
         </a>
       </footer>
     </div>
